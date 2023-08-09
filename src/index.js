da0fe10b2d3e881dfd8d6f6b49595346ccd05b74
devinxiaodai <5063233@qq.com>
2023-08-09 09:10:36 +0800

    d

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { DatePicker } from 'antd';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <StrictMode> 
+    <StrictMode>  
         <Cardlist />
         <DatePicker />
   
